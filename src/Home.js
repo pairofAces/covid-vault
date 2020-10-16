b08ddde9cfd55132597d158af21b127504ece5ea
pairofAces <karanchauhan16@gmail.com>
2020-10-16 00:09:43 -0400

    don't need Home.css file

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Home.css';
 import space from './space.jpg'
 import ReactGlobe from 'react-globe';
 
@@ -9,8 +8,6 @@ function Home() {
 
      return (
         <div className="pulse">
-            {/* <h1>This is the Home Page</h1> */}
-            {/* <ReactGlobe/> */}
             <span></span>
             <span></span>
         </div>
