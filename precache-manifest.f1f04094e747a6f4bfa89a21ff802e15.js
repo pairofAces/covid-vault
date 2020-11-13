a53b596bf4d83681903ddced621ffe74d7e72a5a
pairofAces <karanchauhan16@gmail.com>
2020-11-13 16:01:36 -0500

    Updates

---
+++ precache-manifest.f1f04094e747a6f4bfa89a21ff802e15.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e60b78dea12c700b3d1861e2551d42d",
+    "url": "/covid-vault-client/index.html"
+  },
+  {
+    "revision": "53d0d12f52f61a92b16b",
+    "url": "/covid-vault-client/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "7522fe898bdd706ea81d",
+    "url": "/covid-vault-client/static/css/main.d01118d7.chunk.css"
+  },
+  {
+    "revision": "53d0d12f52f61a92b16b",
+    "url": "/covid-vault-client/static/js/2.f3c92c2a.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/covid-vault-client/static/js/2.f3c92c2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7522fe898bdd706ea81d",
+    "url": "/covid-vault-client/static/js/main.0db44f39.chunk.js"
+  },
+  {
+    "revision": "33ffe167b9cc8a89c22c",
+    "url": "/covid-vault-client/static/js/runtime-main.33f56085.js"
+  },
+  {
+    "revision": "650b82f250587a0061254484b53a0c12",
+    "url": "/covid-vault-client/static/media/CovidVault.650b82f2.png"
+  },
+  {
+    "revision": "0c315261a0a4495369a40931df123787",
+    "url": "/covid-vault-client/static/media/blue-space.0c315261.jpg"
+  },
+  {
+    "revision": "4b69b94abe178f8a6543f89b76607133",
+    "url": "/covid-vault-client/static/media/covid-19.4b69b94a.png"
+  },
+  {
+    "revision": "3d7c025a08fa8637bb533d248115d3e2",
+    "url": "/covid-vault-client/static/media/earth.3d7c025a.png"
+  },
+  {
+    "revision": "0984040eef9233c2763369c6eb47b7f4",
+    "url": "/covid-vault-client/static/media/empty-heart.0984040e.svg"
+  },
+  {
+    "revision": "88bf2af298cb2a7a61550518719e6d96",
+    "url": "/covid-vault-client/static/media/filled-heart.88bf2af2.svg"
+  },
+  {
+    "revision": "897083e40d3edbea2a67fc053e646554",
+    "url": "/covid-vault-client/static/media/me.897083e4.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-vault/precache-manifest.5603f66cc29d6f63c1b44363822b3a01.js"
+  "/covid-vault-client/precache-manifest.f1f04094e747a6f4bfa89a21ff802e15.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid-vault/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid-vault-client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
